db27f808a997e77b12e266abf16f5264ca1f1861
Rajesh Kumar <trajeshkumar37@gmail.com>
2022-08-11 04:15:32 +0000

    Today's Date

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+module.exports = app.get("/", (request, response) => {
+  const date = new Date();
+  response.send(
+    `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+  );
+});
+
+app.listen(3000);
